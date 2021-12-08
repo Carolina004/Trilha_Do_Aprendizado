666eedd2f23e7e13b30b9e47669793d91ad6509b
Carolina004 <88690506+Carolina004@users.noreply.github.com>
2021-12-08 11:42:22 -0300

    Introduzindo JavaScript

---
+++ javascript/js/JS ex04.js
@@ -0,0 +1,7 @@
+var a, b, c
+
+a=8
+b=4
+c=a+b
+m="o resultado da soma é"
+alert(m+c)
---
+++ javascript/js/ex05.js
@@ -0,0 +1,17 @@
+var n1, n2, soma
+
+n1=prompt("Digite o primeiro valor: ")
+n2=prompt("Digite o segundo valor: ")
+
+soma=parseInt(n1)+parseInt(n2)
+
+alert("A soma é " +soma)
+
+var n1, n2, subtração
+
+n1=prompt("Digite o valor de n1: ")
+n2=prompt("Digite o valor de n2: ")
+
+subtração=parseInt(n1)-parseInt(n2)
+
+alert("A subtração é "+subtração)
