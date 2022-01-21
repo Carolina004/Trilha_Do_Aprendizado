eadf0938f165d5fe02780cb6689a9e236b720684
Carolina004 <88690506+Carolina004@users.noreply.github.com>
2022-01-21 11:36:15 -0300

    mais

---
+++ javascript/checar.js
@@ -0,0 +1,12 @@
+function paridade(){
+    var n=parseInt(document.getElementById("n").value);
+    var multi=parseInt(document.getElementById("multi").value);
+    var resposta=document.getElementById('resposta');
+
+    if(n%multi==0){
+        resposta.innerHTML=n+"é múltiplo de"+multi;
+    }
+    else{
+        resposta.innerHTML=n+"não é múltiplo de"+multi;
+    }
+}
